e17751ca1ae2d1758ced3794be3b69a9b6f5d8bd
RaghavRaju22 <raghavraju2204@gmail.com>
2024-07-05 16:59:37 +0530

    update final

--- task1.java
+++ task1.java
@@ -2,10 +2,12 @@ import java.util.*;
 class task1
 {
     public static void main(String[] args) {
-        int i,num,guess;
+        int i,num,guess,ch=1,round=0;
         Random rand=new Random();
         num=rand.nextInt(100) + 1;
         Scanner sc=new Scanner(System.in);
+        while(ch==1)
+        {
         System.out.println("WE HAVE SELECTED A NUMBER BETWEEN 1 AND 100\nGUESS THE NUMBER.(TOTAL 5 CHANCES)");
         for(i=0;i<5;i++)
         {
@@ -13,7 +15,10 @@ class task1
             guess=sc.nextInt();
             if(num==guess)
             {
+                round++;
                 System.out.println("Congratulations, you have guessed the right number.");
+                System.out.println("Your Score : "+(5-i));
+                System.out.println("Rounds Won : "+round);
                 break;
             }
             else if (guess>num) {
@@ -42,5 +47,8 @@ class task1
         {
             System.out.println("SORRY, You used all your chances.");
         }
+        System.out.println("DO YOU WANT TO PLAY AGAIN (1:YES 0:NO) : ");
+        ch=sc.nextInt();
+    }
     }
 }
